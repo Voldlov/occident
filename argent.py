e783f66234f53f39821cfdfe3bd46e35cc96c9de
Lucas GAIO <lucas.gaildossantos@gmail.com>
2022-03-23 10:12:48 +0100

    ajout de méthode init, ajout et suppression

--- argent.py
+++ argent.py
@@ -1,3 +1,16 @@
+"""
+    Classe dédié à l'argent des joueurs
+"""
+
 class argent:
     def __init__(self) -> None:
+        # Initialisation
+        porteMonnaie = 100
+        pass
+    def ajout(self):
+        # Méthode d'ajout de l'argent
+        pass
+    def suppression(self, supp):
+        # Méthode de suppression de l'argent
+
         pass
